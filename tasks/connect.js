3a4be22582991ae4928771a0cd2c9c5a0a3c569c
Jonas Pommerening <jonas.pommerening@gmail.com>
2014-09-30 15:59:40 +0200

    wip

--- tasks/connect.js
+++ tasks/connect.js
@@ -7,29 +7,39 @@ module.exports = function( grunt ) {
    'use strict';
 
    var xcors = require( 'connect-xcors' );
+   var livereload = require( 'connect-livereload' );
    var load = require( './lib/load' );
    var _ = grunt.util._;
 
    var options = grunt.config( 'connect.options' ) || {};
-   var livereload = options.livereload || grunt.config( 'watch.options.livereload' ) || true;
+
+   options.livereload = options.livereload || grunt.config( 'watch.options.livereload' ) || true;
 
    grunt.config( 'watch.options.livereload', livereload );
    grunt.config( 'connect.options', _.defaults( {
       middleware: function( connect, options, middlewares ) {
          var directory = options.directory || options.base[options.base.length - 1];
-         // IMPORTANT: Don't touch the order in which the middleware modules are loaded unless you really
-         // know what you're doing. Otherwise things like setting CORS headers might break.
-         return [
-            xcors( {} ),
+         // override the directory configuration from grunt-contrib-connect with better options
+         middlewares.unshift( connect.directory(directory, { view: 'details', icons: true } ) );
 
-            // (#11) overwriting the directory configuration from grunt-contrib-connect with better options
-            connect.directory(directory, { view: 'details', icons: true } )
-         ].concat( middlewares );
-      }
+         // use different livereload settings
+         if( options.livereload ) {
+            middlewares.unshift( livereload( {
+               port: options.livereload === true ? 35729 : options.livereload,
+               exclude: [ /\.scss$/ ]
+            } ) );
+         }
+
+         // IMPORTANT: Make sure the xcors middleware is the first in the middleware stack,
+         //            otherwise things like setting CORS headers might break.
+         middlewares.unshift( xcors( {} ) );
+
+         return middlewares;
+      },
+      livereload: false
    }, options, {
       hostname: '*',
-      port: 8000,
-      livereload: livereload
+      port: 8000
    } ) );
 
    load( grunt, 'grunt-contrib-connect' );
